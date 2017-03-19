662fad03dcdff44e07b43e33b75bee9fa3876288
hania <hania.rudy@gmail.com>
2017-03-19 14:55:41 +0100

    objects

--- 1/app.js
+++ 1/app.js
@@ -1,24 +1,17 @@
-// Modyfikacja this bind()
-function logNameAll(arg) {
-    console.log(arg + ": " + this.name);
+// Obiekty - dzialania modyfikacje
 
-}
-var person1 = {
-    name: "piotr"
+var person 1= {
+  name: "Pawel"
 };
 
-var person2 = {
-    name: "Michel"
-};
+var person2 = new Object();
 
-var name = "Michelle";
+//operacje dodania [[Put]]
+person2.name = "Beata";
+person1.age = 22;
+person2.age = 33;
 
-//tworzy funkcje powiazana z obiektem person1
-var logNameAll1 = logNameAll.bind(person1);
-console.log(logNameAll1('person1'));
-var logNameAll2 = logNameAll.bind(person2, 'person2');
-console.log(logNameAll2());
 
-person2.logName = logNameAll1;
-console.log(person2.logName('person2'))
-// czyli bez stringa kontekst czyli this a string to parametr czyli arg
+//modyfikacja [[Set]]
+person1.name = "Grzes";
+person2.name = "Rafal";
