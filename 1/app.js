e7af6e8d0d7d444b4fe1d8cdfc1ba66ae539147a
hania <hania.rudy@gmail.com>
2017-03-19 11:52:24 +0100

    function declarations

--- 1/app.js
+++ 1/app.js
@@ -1,3 +1,19 @@
 // Funkcje
 
 //Właściwosc typu Function [[Call]]
+//deklaracja funckji
+var sum = add(3, 4);
+console.log(sum); //7
+
+function add(a, b) {
+  return a + b;
+}
+
+//wyrazenie funkcji
+var result = add(3, 4);
+console.log(result);
+var add = function(a, b) {
+  return a+ b;
+};
+
+//
