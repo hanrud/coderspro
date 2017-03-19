542f488f67547a657532b2a416d3f4410a70486a
hania <hania.rudy@gmail.com>
2017-03-19 09:56:52 +0100

    functions and arrays literals

--- 1/app.js
+++ 1/app.js
@@ -1,17 +1,19 @@
-// var obj = new Object();
-// //roznica miedzy obiektem a klasa - obiekt to instancja klasy
-// //obiekt nieuporzadkowana lista jakis wlasciwosci
-// // funkcje w obiektach to metody
-// //3 sposoby tworzenia
-// //konstrukrtor to n gorze
-// //iterał:
-// var obj1 = {};
-// var obj2 = Object.prototype.constructor;
-// var obj1 = obj;
-//
-// obj = null; //garbage collection - odsmiecanie pamięci
+// literały obiektow i tablic
+var car = {
+  manufacturer: "ford",
+  model: "mondeo"
+};
+ //tak lepiej jak na gorze
+var car = {
+  'manufacturer origin': "ford", //tak można ale tak nie robic lepiej camelcase
+  model: "mondeo"
+};
 
+var car2;
+car2.manufacturer = "fiat";
+car2.model = '500';
 
-var obj = new Object();
-obj.name = "abc"; //własciwości obiektow
-var obj1 = obj;
+//literał tablic
+
+var arr = ['red', 'green', 'blue'];
+console.log(arr[1]);
