d905716ee988f0ee097575ab40a4bfce722a57bc
hania <hania.rudy@gmail.com>
2017-03-19 14:32:33 +0100

    this bind()

--- 1/app.js
+++ 1/app.js
@@ -1,4 +1,4 @@
-// Modyfikacja this apply()
+// Modyfikacja this bind()
 function logNameAll(arg) {
     console.log(arg + ": " + this.name);
 
@@ -13,6 +13,12 @@ var person2 = {
 
 var name = "Michelle";
 
-logNameAll.apply(this, ["global"]); //global: Michellle 
-logNameAll.apply(person1, ["person1"]); //person1: Piotr
-logNameAll.apply(person2, ["person2"] ); // person2: Michel
+//tworzy funkcje powiazana z obiektem person1
+var logNameAll1 = logNameAll.bind(person1);
+console.log(logNameAll1('person1'));
+var logNameAll2 = logNameAll.bind(person2, 'person2');
+console.log(logNameAll2());
+
+person2.logName = logNameAll1;
+console.log(person2.logName('person2'))
+// czyli bez stringa kontekst czyli this a string to parametr czyli arg
