ca3b1078101106af5677cc1a832e6979970a94db
hania <hania.rudy@gmail.com>
2017-03-19 09:22:21 +0100

    start

---
+++ 1/app.js
@@ -0,0 +1,17 @@
+// var obj = new Object();
+// //roznica miedzy obiektem a klasa - obiekt to instancja klasy
+// //obiekt nieuporzadkowana lista jakis wlasciwosci
+// // funkcje w obiektach to metody
+// //3 sposoby tworzenia
+// //konstrukrtor to n gorze
+// //iterał:
+// var obj1 = {};
+// var obj2 = Object.prototype.constructor;
+// var obj1 = obj;
+//
+// obj = null; //garbage collection - odsmiecanie pamięci
+
+
+var obj = new Object();
+obj.name = "abc"; //własciwości obiektow
+var obj1 = obj;
