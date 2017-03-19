bb703eefb71040b13b6d91978dc675e1ee46d1f2
hania <hania.rudy@gmail.com>
2017-03-19 13:12:33 +0100

    parameters of functions

--- 1/app.js
+++ 1/app.js
@@ -1,15 +1,36 @@
-// Funkcje jako wartośc
+//Parametry funkcji
 
-function sayHello() {
-  console.log("Hi");
+function show(value) {
+  return value;
 }
 
-sayHello();
-var sayHello2 = sayHello;
+//Array.isArray(argument)  - false
 
-var numbers = [1,45,2,4,45,6,78];
-var foo = numbers.sort(function(a, b) {
-  return a - b;
-});
+console.log(show("Hi")); //Hi
+console.log(show("Hi", 34)); //Hi
+console.log(show.length); //1 - funkcja oczekuje 1 parametru - arnośc funkcji
 
-console.log(foo);
+show = function() {
+  return arguments[0];
+}
+
+console.log(show("Hello")); // Hello
+console.log(show("Hello", 45)); //hello
+console.log(show.length); //0
+
+function sum() {
+  var result = 0;
+  var len = arguments.length;
+  var i = 0;
+
+  while ( i < len) {
+    result += arguments[i];
+    i++;
+  }
+  return result;
+}
+
+console.log(sum(1,5)); //6
+console.log(sum(4,5,7)); //16
+console.log(sum(3)); //3
+console.log(sum()); //0
