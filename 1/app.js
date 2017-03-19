51d9fcc1856d5f7f9893b6e8774b820b95d7e4ff
hania <hania.rudy@gmail.com>
2017-03-19 15:22:28 +0100

    delete object property

--- 1/app.js
+++ 1/app.js
@@ -1,39 +1,10 @@
-// Obiekty - wykrywanie własciwosci
+// Obiekty - usuwanie wartosci
 
-//NIE!
-/*var person = {
-  number: 0
+var person = {
+  name: "Hania"
 };
 
-if (person.number) {
-  console.log("true");
-} else {
-  console.log("false");
-} //falses*/
-
-// var person1 = {
-//   name: "Hania",
-//   age: 22
-// };
-//
-// var person2 = {
-//   name: "Lukasz",
-//   age: 25
-// };
-//
-// console.log('name' in person1); // true
-// console.log('age' in person2); //true
-// console.log('height' in person2); //falses
-
-
-var person1 = {
-  name: "Hania",
-  logName: function() {
-    console.log(this.name);
-  }
-};
-
-console.log('name' in person1); //true
-console.log(person1.hasOwnProperty("name")); //true
-console.log('toString' in person1); //true jezeli nie znajdzie w obiekcie to szuka w rodzicu dlatego true
-console.log(person1.hasOwnProperty("toString")); //false sprawdza czy dana instancja posiada ta metode
+console.log(person.name); //Hania
+delete person.name;   
+console.log("name" in person); //false
+console.log(person.name); //undefind
