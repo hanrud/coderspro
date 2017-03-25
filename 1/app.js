f1ffdaceceea4f57888e1d2cff02e411f0df8cb6
hania <hania.rudy@gmail.com>
2017-03-25 14:07:35 +0100

    modifying prototypes

--- 1/app.js
+++ 1/app.js
@@ -1,52 +1,19 @@
-//
+
+
 
 (function() {
   function Person(name) { //name to parametr, atrybut to wartosc
     this.name = name;
   }
 
-  Person.prototype.sayHallo = function() {
-    console.log("Hello %s!", this.name)
+  var person1 = new Person("Zosia");
+  var person2 = new Person("Tosia");
+
+  Object.freeze(person1);
+
+  Person.prototype.sayHi = function() {
+    console.log("Hi");
   };
-
-  var person1 = new Person("Ula");
-  var person2 = new Person("Maria");
-
-  console.log(person1.name, person2.name);
-  person1.sayHello();
-  person2.sayHello();
-})();
-//
-
-(function() {
-  function Person(name) { //name to parametr, atrybut to wartosc
-    this.name = name;
-  }
-
-  Person.prototype.arrPerson = [];
-
-  var person1 = new Person("Ula");
-  var person2 = new Person("Maria");
-
-  person1.arrPerson.push("czerwony");
-  person2.arrPerson.push("niebieski");
-
-  console.log(person1.arrPerson); //['czerwony', 'niebieski']
-  console.log(person2.arrPerson); //['czerwony', 'niebieski']
-})();
-
-
-(function() {
-  function Person(name) { //name to parametr, atrybut to wartosc
-    this.name = name;
-  }
-//definiowanie wielu metod naraz w prototypie
-  Person.prototype = {
-    sayHello: function() {
-      //asas
-    },
-    toString: function() {
-      //ask
-    }
-  }
+  person1.sayHi(); //Hi
+  person2.sayHi(); //Hi
 })();
