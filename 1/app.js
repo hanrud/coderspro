b5f56ce27942101469d19f9bb6b41adb80eb9247
hania <hania.rudy@gmail.com>
2017-03-25 13:53:11 +0100

    prototype methods

--- 1/app.js
+++ 1/app.js
@@ -1,45 +1,52 @@
-// Konstruktory i prototypy
-
-function Computer() {
-    //
-}
-
-//new zwraca wartośc
-var computer1 = new Computer;
-var computer2 = new Computer;
-
-console.log(computer1.constructor === Computer); //true
-/////
-
-function Person(name) {
-    this.name = name;
-    this.sayHello = function() {
-        console.log("hello");
-    };
-}
-
-var person1 = new Person("Piotr");
-var person2 = new Person("Pawel");
-console.log(person1.name); //Piotr
+//
 
 (function() {
-    "use strict";
+  function Person(name) { //name to parametr, atrybut to wartosc
+    this.name = name;
+  }
 
-    function Car(name) {
-        Object.defineProperty(this, 'name', { //to jest to samo co this.name na gorze
-            get: function() {
-                return name;
-            },
-            set: function(value) {
-                name = value;
-            }
-        });
-            this.brrrum = function() {
-              console.log("Bruuuuum");
-            }
+  Person.prototype.sayHallo = function() {
+    console.log("Hello %s!", this.name)
+  };
 
+  var person1 = new Person("Ula");
+  var person2 = new Person("Maria");
+
+  console.log(person1.name, person2.name);
+  person1.sayHello();
+  person2.sayHello();
+})();
+//
+
+(function() {
+  function Person(name) { //name to parametr, atrybut to wartosc
+    this.name = name;
+  }
+
+  Person.prototype.arrPerson = [];
+
+  var person1 = new Person("Ula");
+  var person2 = new Person("Maria");
+
+  person1.arrPerson.push("czerwony");
+  person2.arrPerson.push("niebieski");
+
+  console.log(person1.arrPerson); //['czerwony', 'niebieski']
+  console.log(person2.arrPerson); //['czerwony', 'niebieski']
+})();
+
+
+(function() {
+  function Person(name) { //name to parametr, atrybut to wartosc
+    this.name = name;
+  }
+//definiowanie wielu metod naraz w prototypie
+  Person.prototype = {
+    sayHello: function() {
+      //asas
+    },
+    toString: function() {
+      //ask
     }
-
-    var car1 = new Car("Szybki");
-    car1.brrrum(); //"Bruuuuum"
+  }
 })();
