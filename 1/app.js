7074569533bfb6415a6d9d97a93a240a804d226a
hania <hania.rudy@gmail.com>
2017-03-25 19:47:58 +0100

    sort by element length

--- 1/app.js
+++ 1/app.js
@@ -1,43 +1,17 @@
 (function() {
-    function accumulator(str) {
-        var array = [];
+    function sortStrings(array){
         var newArray = [];
-        var newStr = '';
-        array = str.split('');
-        for (var i = 0; i < array.length; i++) {
-            newArray.push(array[i].repeat(i+1));
-                    }
-        for (var j = 0; j < newArray.length; j++) {
-            newArray[j] = newArray[j].charAt(0).toUpperCase() + newArray[j].split(1);
-        }
-           newStr = newArray.join("-");
-        return newStr;
-    }
-console.log(accumulator("abcd"));
+    for (var i=0; i < array.length; i++ ) {
+       newArray.push(array[i].replace(/[^A-Z]/gi, "")) ;
+    };
+     newArray.sort(function(a,b) {
+         return a.length - b.length;
+     })
+
+       return newArray;
+   }
+
+   console.log(sortStrings(['fds223aas', 'fd!/ss', 'fds2df/ff', 'fdasssdf', 'a', 'ah23jcz!!!l']));
 })();
 
-(function() {
-    function pin(str) {
-        var array = [];
-        var newArray = [];
-        var total =0;
-        array = str.split('');
-        for (i = 0; i < array.length; i++) {
-            newArray.push(parseInt(array[i]));
-        }
-        for (var j =0; j < newArray.length; j++) {
-            if (typeof newArray[j] == 'number') {
-               total +=1;
-            }
-        }
-        if (newArray.length == 4 && total == 4) {
-            console.log('to jest pin');
-        } else {
-            console.log("to nie jest pin");
-        }
-        return newArray;
-    }
-console.log(pin("1256"));
-console.log(pin("1d2sdd56"));
-console.log(pin("1!2223vd2   56"));
-})();
+//posortowac i usunac wszystkie znaki oprocz liter
