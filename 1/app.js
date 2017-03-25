44cc47391542a8fc6f4fc70f1c23922a42a79d50
hania <hania.rudy@gmail.com>
2017-03-25 11:42:06 +0100

    seal obj

--- 1/app.js
+++ 1/app.js
@@ -1,24 +1,47 @@
 // Atrybuty wspolne
 
-// [[Enumerable]], [[Configuarable]]
+// [[Value]], [[Writeable]]
+// iife
+(function() {
+    'use strict';
 
-// Do zmiany tych wlasciwosci uzywamy metody Object.defineProperty()
+    var person = {
+      name: "Konrad"
+    };
+})();
+
 
 (function() {
     'use strict';
+    var person = {};
+    Object.defineProperty(person, 'name', {
+      value: "Pawel",
+      enumerable: true,
+      configuarable: true,
+      writable: true
+    });
+})();
+//Object.seal() zapieczętowanie
+//extensible i configurable  ustawia na false
+(function() {
+    'use strict';
     var person = {
-        name: 'Ula'
+      name: "Konrad"
     };
-    console.log(person.propertyIsEnumerable("name")); //true
-    Object.defineProperty(person, "name", {
-        configurable: false
-    });
+    console.log(Object.isExtensible(person)); //true
+    console.log(Object.isSealed(person)); //false
 
-    Object.defineProperty(person, "name", {
-        enumerable: false
-    });
-    console.log(person.propertyIsEnumerable("name"));
-    Object.defineProperty(person, "name", {
-        configurable: true
-    });
+    Object.seal(person);
+    console.log(Object.isExtensible(person)); //false
+    console.log(Object.isSealed(person)); //true
+
+    person.sayHello = function() {
+      console.log(this.name);
+    };
+
+    //sprawdzimy istnienie metody
+    console.log("sayHello" in person); //false
+
+    person.name = 'abcd';
+    console.log(person.name); // "Konrad"
 })();
