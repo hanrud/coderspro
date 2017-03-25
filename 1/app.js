605f09bd87d865153a6523a571d1106851f7318f
hania <hania.rudy@gmail.com>
2017-03-25 19:17:06 +0100

    excercises pin and ABbCcc

--- 1/app.js
+++ 1/app.js
@@ -1,48 +1,43 @@
 (function() {
-    function Person(name) { //name to parametr, atrybut to wartosc
-        this.name = name;
-    }
-
-    var person1 = new Person("Zosia");
-    var person2 = new Person("Tosia");
-
-    Object.freeze(person1);
-
-    Person.prototype.sayHi = function() {
-        console.log("Hi");
-    };
-    person1.sayHi(); //Hi
-    person2.sayHi(); //Hi
-})();
-
-
-(function() {
-    Array.prototype.sumUp = function() { //sumuje wszystkie elementy tablicy
-        return this.reduce(function(a, b) {
-            return a + b;
-        }, 0);
-    };
-})();
-
-(function() {
-    Array.prototype.sumUp1 = function() {
-      var total = 0;
-      var array = [];
-      var array1 = [];
-      array1 = this.sort(function(a,b) {
-        return a - b;
-      });
-        array.push(array1[0]);
-      for (var i = 1; i < array1.length; i++) {
-        if (array1[i-1] != array1[i]){
-          array.push(array1[i]);
+    function accumulator(str) {
+        var array = [];
+        var newArray = [];
+        var newStr = '';
+        array = str.split('');
+        for (var i = 0; i < array.length; i++) {
+            newArray.push(array[i].repeat(i+1));
+                    }
+        for (var j = 0; j < newArray.length; j++) {
+            newArray[j] = newArray[j].charAt(0).toUpperCase() + newArray[j].split(1);
         }
-      };
-      for (var j = 0; j < array.length; j++) {
-        total += array[j];
-      };
-      return total;
-    };
-      var numbers = [1,2,5,5,2,2,3,4] //15
-      console.log(numbers.sumUp1());
+           newStr = newArray.join("-");
+        return newStr;
+    }
+console.log(accumulator("abcd"));
+})();
+
+(function() {
+    function pin(str) {
+        var array = [];
+        var newArray = [];
+        var total =0;
+        array = str.split('');
+        for (i = 0; i < array.length; i++) {
+            newArray.push(parseInt(array[i]));
+        }
+        for (var j =0; j < newArray.length; j++) {
+            if (typeof newArray[j] == 'number') {
+               total +=1;
+            }
+        }
+        if (newArray.length == 4 && total == 4) {
+            console.log('to jest pin');
+        } else {
+            console.log("to nie jest pin");
+        }
+        return newArray;
+    }
+console.log(pin("1256"));
+console.log(pin("1d2sdd56"));
+console.log(pin("1!2223vd2   56"));
 })();
