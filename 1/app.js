92e540773df1bb293caffbf6283c577eee166f7c
hania <hania.rudy@gmail.com>
2017-03-26 09:53:09 +0200

    methods inherited after Object.prototype

--- 1/app.js
+++ 1/app.js
@@ -1,9 +1,38 @@
-//Dziedziczenie - (prototype chaining)
+// Metody dziedziczone po Object.prototype
 
+// - hasOwnProperty()
+// - propertyIsEnumerable()
+// - isPrototypeOf()
+// - valueOf() - zwraca wartosc
+// - toString()
+
+//
+//valueOf()
+var now = new Date();
+var earlier = new Date(2017, 01, 01); //2017-01-01
+
+console.log(now > earlier);
+
+var string = "Hello world";
+var response = string.valueOf();
+console.log(response); // Hello world
+
+//toString()
 var book = {
-    title: "ABcd"
+  title: "Abcd"
 };
 
-var prototype = Object.getPrototypeOf(book);
+var message = 'Book ' + book;
+console.log(message);//book [object Object]
 
-console.log(prototype == Object.prototype); //true
+//
+
+var book = {
+  title: "Abcd",
+  toString: function() {
+    return '[Book with title of ' + this.title + ']'
+  }
+};
+
+var message = 'Book = ' + book;
+console.log(message); //[Book with title of Abcd]
