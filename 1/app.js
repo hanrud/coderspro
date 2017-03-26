a691797283c99018fd2bdb1a6ef9d2a1caa345da
hania <hania.rudy@gmail.com>
2017-03-26 13:01:29 +0200

    singleton

--- 1/app.js
+++ 1/app.js
@@ -1,34 +1,32 @@
-// Przywlaszczanie konstruktorow
+// Singleton
 
-function Rectangle(length, width)  {
-  this.length = length;
-  this.width = width;
+var Singleton = (function() {
+    console.count();
+
+    var instance;
+
+    function createInstance() {
+      var obj = new Object('Zostalem stworzony');
+      return obj;
+    }
+
+    return {
+      getInstance: function() {
+        if (!instance) {
+          console.log("Zwracam instancje");
+          createInstance();
+        }
+        return instance;
+      }
+    }
+
+})();
+
+function init() {
+  var instance1 = Singleton.getInstance(); //kazda instancja tutaj to to samo
+  var instance2 = Singleton.getInstance();
+
+  console.log(instance1 === instance2); //true
 }
 
-Rectangle.prototype.getArea = function() {
-  return this.length * this.width;
-};
-
-Rectangle.prototype.toString = function() {
-  return 'Rectangle ' + this.length + ' x ' + this.width;
-};
-
-function Square(size) {
-  Rectangle.call(this, size, size);
-}
-
-Square.prototype = Object.create(Rectangle.prototype, {
-  constructor: {
-    value: Square
-  }
-});
-
-Square.prototype.toString = function() {
-  return 'Square '+ this.length + ' x ' + this.width;
-};
-
-var square = new Square(6);
-console.log(square.length);
-console.log(square.width);
-console.log(square.getArea());
-console.log(square.toString());
+init();
