a10a9e9d454c33279033c2b425f2f2606b5a0868
hania <hania.rudy@gmail.com>
2017-03-26 10:48:14 +0200

    inheretance

--- 1/app.js
+++ 1/app.js
@@ -1,21 +1,48 @@
-// Modyfikowanie prototypow
-
-Object.prototype.add = function(val) {
-    return this + val;
-};
+// Dziedziczenie obiektow
 
 var book = {
-    title: "Abcd"
+  title: "Abcd"
 };
 
-console.log(book.add(9)); //[object Object]9
-console.log('tytul '.add(' ksiazki')); //tytul ksiazki
-console.log(document.add(true)); //[object HTMLDocument]true
+var book1 = Object.create(Object.prototype, {
+  title: {
+    value: 'Some another title',
+    enumerable: true,
+    configurable: true,
+    writable: true
+  }
+});
 
-// zabezpieceie
-var empty = {};
-for (var property in empty) {
-    if (empty.hasOwnPropeerty(property)) {
-        console.log(property);
-    }
-}
+console.log(book);
+console.log(book1);
+
+//
+var person1 = {
+  name:  'Zuzia',
+  sayHello: function() {
+    console.log(this.name);
+  }
+};
+
+var person2 = Object.create(person1, {
+  name: {
+    value: 'Ola',
+    enumerable: true,
+    configurable: true,
+    writable: true
+  }
+});
+
+person1.sayHello(); //'Zuzia'
+person2.sayHello(); // "Ola"
+
+console.log(person1.hasOwnProperty("sayHello")); //true
+console.log(person1.isPrototypeOf(person2)); //true
+console.log(person2.hasOwnProperty("sayHello")); //false
+console.log(person2.isPrototypeOf(person2)); //false
+
+//
+// unikanie kolizji nazw - konstruktor 'null'
+var emptyObject = Object.create(null);
+console.log('toString' in emptyObject); //false
+console.log('valueOf' in emptyObject); //false
