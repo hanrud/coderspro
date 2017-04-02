ada48f4c5ddd27d02da3ac5fc9d05e80c212f668
hania <hania.rudy@gmail.com>
2017-04-02 19:55:37 +0200

    function with refactor

--- 1/app.js
+++ 1/app.js
@@ -1,31 +1,38 @@
-//
+// Przy podanej liście numerów, zwrócić nową listę, która zawiera każdy numer z pierwszej przy założeniu
+// że występuje maksymalnie n-razy:
+function naszaFunkcja(array, num) {
+    var newArray = [];
+    var count = [];
 
-var Person = (function() {
-  var age = 23;
+    function searchCount() {
+        for (var x = 0; x < count.length; x++) {
+            if (count[x][0] === array[i]) {
+                if (count[x][1] < num) {
+                    newArray.push(array[i]);
+                    count[x][1] += 1;
+                }
+            }
+        }
+        return x;
+    }
 
-  function InnerPerson(name) {
-    this.name = name;
-  }
+    for (var i = 0; i < array.length; i++) {
+        if (array[i] != null) {
+            if (newArray.indexOf(array[i]) === -1) {
+                newArray.push(array[i]);
+                count.push([array[i], 1]);
+            } else if (newArray.indexOf(array[i]) > -1) {
+                var x = searchCount();
+            }
+        }
+    }
 
-  InnerPerson.prototype.getAge = function () {
-    return age;
-  };
+    return newArray;
 
-  InnerPerson.prototype.growOlder = function () {
-    age++;
-  };
-
-  return InnerPerson;
-})();
-
-var person1 = new Person('pawel');
-var person2 = new Person("gawel");
-console.log(person1.name);
-console.log(person1.getAge());
-
-console.log(person2.name);
-console.log(person2.getAge());
-
-person1.growOlder();
-console.log(person1.getAge());
-console.log(person2.getAge());
+}
+console.log(naszaFunkcja([2, 6, 7, 2, 7, 2, , 9, 2], 2)); // [2, 6, 7, 2, 7, 9]
+console.log(naszaFunkcja([2, 6, 7, 2, 7, 2, , 9, 2], 1)); // [2, 6, 7, 9]
+console.log(naszaFunkcja([2, 6, 7, 2, 7, 2, , 9, 2], 3)); // [2, 6, 7, 2, 7, 2, 9]
+console.log(naszaFunkcja([1, 1, 1, 1], 3)); // [1,1,1]
+console.log(naszaFunkcja([1, 1, 1, 1], 2)); // [1,1]
+console.log(naszaFunkcja([1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3], 2)); // [1, 1, 2, 2]
